c98e295bbe36ed44531e84a6776dbb5d1ab21e96
michalik-maciej <halniak24@gmail.com>
2021-11-25 17:32:46 +0100

    Add form and filename upload

--- server.js
+++ server.js
@@ -2,12 +2,32 @@ const express = require('express');
 const path = require('path');
 const app = express();
 const hbs = require('express-handlebars');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
 
+
+/* Initial config */
 app.engine('hbs', hbs());
 app.set('view engine', 'hbs');
-
+app.use(express.urlencoded({ extended: false })); //config for Postman
 app.use(express.static(path.join(__dirname, '/public')));
+app.use(express.json());
 
+
+/* Post requests */
+app.post('/contact/send-message', upload.single('projectFile'), (req, res) => {
+  const { author, sender, title, message } = req.body;
+
+  if (author && sender && title && message && req.file) {
+    res.render('contact', { projectFileName: req.file.originalname, isSent: true });
+  }
+  else {
+    res.render('contact', { isError: true });
+  }
+})
+
+
+/* Get requests */
 app.get('/', (req, res) => {
   res.render('index');
 });
@@ -17,7 +37,7 @@ app.get('/hello/:name', (req, res) => {
 })
 
 app.get('/about', (req, res) => {
-  res.render('about', { layout: 'dark' });
+  res.render('about');
 });
 
 app.get('/contact', (req, res) => {
@@ -32,6 +52,8 @@ app.get('/info', (req, res) => {
   res.render('info');
 });
 
+
+/* Not found response */
 app.use((req, res) => {
   res.status(404).send('404 not found...');
 });
